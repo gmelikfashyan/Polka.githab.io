73e116cabffe0ae3045c25202f07a0657f12e7b5
gmelikfashyan <153018768+gmelikfashyan@users.noreply.github.com>
2023-12-06 00:18:40 +0300

    Delete other/1.py

--- other/1.py
+++
@@ -1,7 +0,0 @@
-from rembg import remove
-from PIL import Image
-input_path = 'puskin_art.jpg'
-output_path = 'pushkin.png'
-input = Image.open(input_path)
-output = remove(input)
-output.save(output_path)
